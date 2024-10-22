f493492e4d3c611e42bf491eb41245b498badb14
Shyne-creator <afachri851@gmail.com>
2024-10-22 22:41:35 +0700

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Ganti dengan alamat email Anda
+    $to = "bimzkaze@gmail.com"; 
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    // Kirim email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Failed to send email.";
+    }
+}
+?>
